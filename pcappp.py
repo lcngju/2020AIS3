e3e8104375ccbf3c98697d35c55b676a8d04db3e
lcngju <chungjulai@gmail.com>
2020-07-30 20:17:24 +0800

    get_MAC

--- pcappp.py
+++ pcappp.py
@@ -4,21 +4,26 @@ from pcapfile.protocols.linklayer import ethernet
 from pcapfile.protocols.network import ip
 import binascii
 
-infile = sys.argv[1]
 
-testcap = open(infile, 'rb')
-capfile = savefile.load_savefile(testcap, verbose=True)
-MACip = dict()
-for idx, packet in enumerate(capfile.packets):
-    try:
-        eth_frame = ethernet.Ethernet(packet.raw())
-        ip_packet = ip.IP(binascii.unhexlify(eth_frame.payload))
-        MAC = eth_frame.src
-        ip_t = ip_packet.src
-        MACip[MAC] = ip_t
-    
-    except:
-        pass
 
-print(MACip)
+def get_MACip(infile):
+    testcap = open(infile, 'rb')
+    capfile = savefile.load_savefile(testcap, verbose=True)
+    MACip = dict()
+    for idx, packet in enumerate(capfile.packets):
+        try:
+            eth_frame = ethernet.Ethernet(packet.raw())
+            ip_packet = ip.IP(binascii.unhexlify(eth_frame.payload))
+            MAC = eth_frame.src
+            ip_t = ip_packet.src
+            MACip[MAC] = ip_t
+            
+        except:
+            pass
 
+    return MACip
+
+if __name__ == '__main__':
+	
+    infile = sys.argv[1]
+    print(get_MACip(infile))
